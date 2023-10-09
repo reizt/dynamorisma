6bdae3b85ad128e4e6d872c7934634a4a1773a69
reizt <reizt.dev@gmail.com>
2023-10-10 08:41:54 +0900

    Introduce rollup

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-export { createDynmr } from './src/client/index';
+export { createDynmr } from './src/client';
 export type { EntConfig, InferEnt, InferProp, PropConfig } from './src/client/types/config';
 export type { AvailableGsiPropName, CollectIn, Dynmr, DynmrIdInfo, DynmrSchema, EntRepo, Filter, PickIn, Where } from './src/client/types/repo';
 export type { Context as DynmrContext } from './src/context';
