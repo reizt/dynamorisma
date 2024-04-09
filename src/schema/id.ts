aef92eb2083786c36cca211caed3b236736db916
Reiju Takahashi <reizt.dev@gmail.com>
2024-04-09 19:14:11 +0900

    Renew id generation

--- src/schema/id.ts
+++ src/schema/id.ts
@@ -1,20 +1,16 @@
-import { randomBytes } from 'node:crypto';
+import { randomInt } from 'node:crypto';
 
 export const dynmrIdAttrName = '__dynmrId';
 export const entNameAttrName = '__entName';
 
-const len = 8;
-
-export const newDynmrId = (): string => {
-  const idChars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-';
-  let buf = randomBytes(len);
-  // randomBytes may return less than len buf
-  while (buf.length < len) {
-    buf = randomBytes(len);
-  }
-  const residues: number[] = [];
+const randomString = (chars: string[], len: number): string => {
+  let string = '';
   for (let i = 0; i < len; i++) {
-    residues.push(buf[i]! & (idChars.length - 1));
+    string += chars[randomInt(0, chars.length)];
   }
-  return residues.map((c) => idChars.charAt(c)).join('');
+  return string;
 };
+
+const chars = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'];
+
+export const newDynmrId = () => randomString(chars, 8);
